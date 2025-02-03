0fb995a5c89bd0be3fc94ead82919a33086045b3
Favour St John <favourstjohn@gmail.com>
2025-02-03 11:43:04 +0000

    Time: 31 ms (43.75%) | Memory: 30.1 MB (30.66%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -2,18 +2,23 @@
 # Floyd's Algorithm
 
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0, 0
+    def findDuplicate(self, nums: List[int]):
+        slow = nums[0]
+        fast = nums[0]
+        
+        # Detect cycle
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
-            
-        slow2 = 0
-        while True:
+
+        # Find entry point of the cycle
+        slow = nums[0]
+        
+        while slow != fast:
             slow = nums[slow]
-            slow2 = nums[slow2]
-            if slow == slow2:
-                return slow
+            fast = nums[fast]
+        
+        return slow
         
