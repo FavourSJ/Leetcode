502c1bc9a36512441d056ac9d7d90064043b1726
Favour St John <favourstjohn@gmail.com>
2025-02-04 22:57:30 +0000

    Time: 3 ms (88.79%) | Memory: 17.9 MB (33.78%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,9 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # Negative numbers and numbers ending with 0 (except 0 itself) can't be palindromes
-        if x < 0:
-            return False
-        
         # Converting the number to a string and check if it reads the same forwards and backwards
         return str(x) == str(x)[::-1]
         
