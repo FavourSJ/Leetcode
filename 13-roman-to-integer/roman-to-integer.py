9bbf5688c5e2ef346669b5cb225965299e9a41fa
Favour St John <favourstjohn@gmail.com>
2025-02-05 08:38:10 +0000

    Time: 5 ms (53.80%) | Memory: 18 MB (17.79%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,34 @@
+# Time complexity O(n)
+# Space complexity O(1)
+
+# hashmap of 7 values representing the roman numerals
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+
+        res = 0
+
+        # for loop to iterate through each value
+        for i in range(len(s)):
+            # condition to check:
+            # (1) number is within boundary
+            # (2) the current number is greater than the next number
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                # if so minus it from answer
+                res -= roman[s[i]]
+                # if not add it to the total
+            else:
+                res += roman[s[i]]
+        # return final result
+        return res
+        
