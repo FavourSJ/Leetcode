eb98e72d023745be29ce0524a61541b11348a0f9
Favour St John <favourstjohn@gmail.com>
2025-03-12 10:48:36 +0000

    Time: 291 ms (29.76%) | Memory: 21.5 MB (33.33%) - LeetSync

---
+++ 2683-count-the-number-of-good-subsequences/count-the-number-of-good-subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countGoodSubsequences(self, s: str) -> int:
+        dict1 = collections.Counter(s)
+
+        m, total = max(dict1.values()), 0
+
+        @lru_cache(1000)
+        def dfs(n, k):
+            return 1 if not k else dfs(n, k - 1) * (n - k + 1) // k
+
+        for k in range(1, m + 1):
+            product = 1
+
+            for i in dict1.values():
+                product *= (1 + dfs(i, k))
+
+            total += product - 1
+
+        return total % (10**9 + 7)
