903355f77a31cd5e785d8eac1b0b22a8697b669c
Favour St John <favourstjohn@gmail.com>
2025-03-23 18:14:53 +0000

    Time: 11 ms (53.92%) | Memory: 31.6 MB (29.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+# creating a set to store values
         hashset = set()
 
+# iterate through the array
         for n in nums:
+# check to see value is already in hashset
             if n in hashset:
+# if so -> TRUE
                 return True
-
+# if not add to hashset and carry on
             hashset.add(n)
+# no duplicates -> FALSE
         return False
-        
