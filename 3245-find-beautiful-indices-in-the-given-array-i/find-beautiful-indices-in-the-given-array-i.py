29890235fb44e3b4330c2efbf7516707f1aa1ed3
Favour St John <favourstjohn@gmail.com>
2025-03-23 16:37:50 +0000

    Time: 108 ms (81.90%) | Memory: 22.1 MB (31.97%) - LeetSync

--- 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.py
+++ 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.py
@@ -1,25 +1,23 @@
-# Time Complexity: O(NM)
-
 class Solution:
     def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
-        # Find all occurrences of `a` in `s`
+        # find all occurences of a in s
         a_indices = [i for i in range(len(s) - len(a) + 1) if s[i:i + len(a)] == a]
 
-        # Find all occurrences of `b` in `s`
+        # find all occurences of b in s
         b_indices = [j for j in range(len(s) - len(b) + 1) if s[j:j + len(b)] == b]
 
-        result = [] # Stores the final beautiful indices
-        j = 0 # Pointer for `b_indices`
+        result = []
+        # pointer for b_indices
+        j = 0 
 
         # Iterate over each index `i` where `a` appears
         for i in a_indices:
             # Move `j` to the first `b` index that is within `k` distance
             while j < len(b_indices) and b_indices[j] < i - k:
                 j += 1
-            
+
             # Check if `b_indices[j]` is within range of `k`
             if j < len(b_indices) and abs(b_indices[j] - i) <= k:
+                # Append `i` to result if it meets the condition
                 result.append(i)
-
         return result
-        
