1e75a510c249e23b14f7cd6380cfdfd7da1f445d
Favour St John <favourstjohn@gmail.com>
2025-03-23 18:09:52 +0000

    Time: 0 ms (100.00%) | Memory: 18.2 MB (55.04%) - LeetSync

--- 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -1,10 +1,12 @@
-# Time Complexity O(n)
-# Space Complexity O(1)
-
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        (x0, y0), (x1, y1) = coordinates[: 2]
+# working out the initial gradient from first 2 points
+        (x0, y0), (x1, y1) = coordinates[:2]
+
+# iterate over all points (coordinates) in the List
         for x, y in coordinates:
             if (x1 - x0) * (y - y1) != (x - x1) * (y1 - y0):
+# if the condition fails, the points don't have the same gradient
                 return False
+# if all points satisfy the condition -> straight line :)
         return True
