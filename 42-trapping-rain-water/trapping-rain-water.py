3b54f22e30f2f41e30cb501e9d497b88701c6318
Favour St John <favourstjohn@gmail.com>
2025-03-30 23:10:51 +0100

    Time: 9 ms (79.90%) | Memory: 19.2 MB (93.06%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,26 @@
+
+# equation min(l, r) - h[i]
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        if not height:
+            return 0
+
+        l = 0
+        r = len(height) - 1
+        leftMax = height[l]
+        rightMax = height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
