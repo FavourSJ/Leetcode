4d80ca73d00a9eb602b63ad179cb64fa88acc76b
Favour St John <favourstjohn@gmail.com>
2025-09-04 10:18:17 +0100

    Time: 17 ms (21.71%) | Memory: 18.6 MB (12.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if (sorted(s) != sorted(t)) or (len(s) != len(t)):
             return False
-        
-        return sorted(s) == sorted(t)
+        else:
+            return True
         
