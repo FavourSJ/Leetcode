c2e8b69a65ad942929f6543ca79baf6471467bbf
Favour St John <favourstjohn@gmail.com>
2025-09-16 19:51:02 +0100

    Time: 0 ms (100.00%) | Memory: 19 MB (6.01%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,6 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+        left = 0
+        right = len(nums) - 1
+ 
+        while left <= right:
+            middle = (right + left) // 2
+ 
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                left = middle + 1
+ 
         return -1
+
+        # Brute Force solution
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
+        # return -1
