c11fd097fd70d5db6cc0215d2725b2cfe03b3135
Favour St John <favourstjohn@gmail.com>
2025-09-17 11:31:17 +0100

    Time: 0 ms (100.00%) | Memory: 18.3 MB (73.27%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,6 +1,26 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i, num in enumerate(nums):
-            if target <= num:
-                return i  # target found or should be inserted here
-        return len(nums)
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            middle = (l + r) // 2
+            if nums[middle] < target:
+                l = middle + 1
+            elif nums[middle] > target:
+                r = middle - 1
+            elif nums[middle] == target:
+                return middle
+        
+        if nums[middle] < target:
+            return middle + 1
+        else:
+            return middle
+
+
+# Brute Force approach
+        # for i, num in enumerate(nums):
+        #     if target <= num:
+        #         return i  # target found or should be inserted here
+        # return len(nums)
