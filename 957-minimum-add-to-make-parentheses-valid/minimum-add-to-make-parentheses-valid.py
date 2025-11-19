faff1af85e556b57137ebc2334ddcc1a528fb755
Favour St John <favourstjohn@gmail.com>
2025-11-19 12:09:50 +0000

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.17%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -6,10 +6,9 @@ class Solution:
         for c in s:
             if c == '(':
                 openP += 1
-            elif c == ')':
-                if openP > 0:
-                    openP -= 1
-                else:
-                    closedP += 1
+            elif c == ')' and openP > 0:
+                openP -= 1
+            else:
+                closedP += 1
         
         return openP + closedP
