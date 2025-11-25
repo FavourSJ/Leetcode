d3f43a328a592cbfe830dfb02f7ea432b71c13f5
Favour St John <favourstjohn@gmail.com>
2025-11-25 19:56:02 +0000

    Time: 11 ms (83.13%) | Memory: 21.3 MB (39.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        # using a dictionary to map a sorted tuple of characters
+        anagrams = defaultdict(list)
 
+        # iterate through the string input
         for s in strs:
-            res[tuple(sorted(s))].append(s)
+            # sort the characters in each string
+            key = tuple(sorted(s))
+            # append the original string to the list corresponding to this key
+            anagrams[key].append(s)
 
-        return list(res.values())
+    # at this point all strings that are anagrams of 's' will share the same key
+
+        # return all the grouped anagrams list
+        return list(anagrams.values())
