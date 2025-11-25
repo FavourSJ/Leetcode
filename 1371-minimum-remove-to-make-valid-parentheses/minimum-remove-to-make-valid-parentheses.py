1f14135cbd9f1028bfb1321b7c1bb85fbb42e542
Favour St John <favourstjohn@gmail.com>
2025-11-25 16:53:25 +0000

    Time: 39 ms (71.45%) | Memory: 21 MB (9.31%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,23 +1,31 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         res = []
+        # count the number of unmatched '('
         count = 0
 
+        # first pass to remove the invalid ')'
         for ch in s:
             if ch == "(":
+                # need to track the unmatched '('
                 res.append(ch)
                 count += 1
             elif ch == ")" and count > 0:
+                # match ')' with an opening '('
                 res.append(ch)
                 count -= 1
             elif ch != ")":
+                # just add normal letters
                 res.append(ch)
 
+        # second passing through remove any extra '('
         filtered = []
+        # iterate through backwards so from right to left
         for ch in res[::-1]:
             if ch == "(" and count > 0:
                 count -= 1
             else:
                 filtered.append(ch)
 
+        # return everything but reverse back to the correct order
         return "".join(filtered[::-1])
