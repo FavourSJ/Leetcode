1740eecaa8af1ce26eaa008dee3f1e3019a2117d
Favour St John <favourstjohn@gmail.com>
2025-11-26 12:44:47 +0000

    Time: 1195 ms (88.64%) | Memory: 56.3 MB (84.53%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,18 +1,28 @@
-# Sieve of Erathosthenes Algorithm
+# Sieve of Eratosthenes Algorithm
 
 class Solution:
     def countPrimes(self, n: int) -> int:
-
+        # if n is less than 2 there are no primes at all
         if n < 2:
             return 0
 
-        isPrime = [True for i in range(n)]
-        isPrime[0] = isPrime[1] = False
+        # create a boolean list where is_prime[i] indicates if i is prime or not
+        # assume every number is prime
+        is_prime = [True] * n
+        # 0 and 1 are not prime
+        is_prime[0] = is_prime[1] = False
 
-        for i in range(2, math.ceil(math.sqrt(n))):
-            if isPrime[i]:
-                for j in range(i*i, n, i):
-                    isPrime[j] = False
+        # start from i = 2
+        i = 2
+        # iterate up until the square root of n becase any composite < n must have a factor
+        while i * i < n:
+            # if i is still marked prime, mark all its multiples as not prime then
+            if is_prime[i]:
+                # marking from i*i, as smaller multiples were already handled
+                for j in range(i * i, n, i):
+                    is_prime[j] = False
 
-        return sum(isPrime)
+            i += 1
+        
+        return sum(is_prime)
         
