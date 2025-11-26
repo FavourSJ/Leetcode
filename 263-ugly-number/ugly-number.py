900bf9cbd51feef6b3502c7be1f1f964b1704eb1
Favour St John <favourstjohn@gmail.com>
2025-11-26 13:37:35 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (41.37%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
+        # ugly numbers must be positive
         if n <= 0:
             return False
-        
+
+        # repeatedly divide n by the allowed prime factors which is 2,3 and 5
         for prime in [2, 3, 5]:
+            # while n is divisible by this prime, keep dividing it, then move to the next
             while n % prime == 0:
                 n = n // prime
+
+        # n must be reduced to 1 to be considered ugly
         if n == 1:
             return True
+
         return False
